de4e51d6ca1547debe2d2f4dc6dce4f623b2563d
kursatyasar <yasarkursat93@gmail.com>
2017-11-18 17:32:37 +0200

    new mode of testing

---
+++ src/test/com/aric/samples/account/controller/AccountControllerTest.java
@@ -0,0 +1,21 @@
+package com.aric.samples.account.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AccountControllerTest {
+    @Test
+    public void account() throws Exception {
+        System.out.println("klaklsdn");
+    }
+
+    @Test
+    public void deposit() throws Exception {
+    }
+
+    @Test
+    public void withdraw() throws Exception {
+    }
+
+}
--- test/com/aric/samples/account/controller/AccountControllerTest.java
+++
@@ -1,7 +0,0 @@
-package com.aric.samples.account.controller;
-
-import static org.junit.Assert.*;
-
-public class AccountControllerTest {
-
-}
